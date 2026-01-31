ef3eddc76d646f33bbcf7ed9f3ca93fb25fbb950
quantumVector <quantum@MacBook-Air-som-tillhor-quantumVector.local>
2026-01-31 19:45:09 +0300

    some update

---
+++ dist/lcp.js
@@ -0,0 +1,28 @@
+"use strict";
+function initApp() {
+    const elements = [
+        document.getElementById('elem-1'),
+        document.getElementById('elem-3'),
+        document.getElementById('elem-4'),
+        document.getElementById('elem-2')
+    ];
+    let currentIndex = 0;
+    const interval = setInterval(() => {
+        if (currentIndex < elements.length) {
+            const elem = elements[currentIndex];
+            if (elem) {
+                elem.style.display = 'block';
+            }
+            currentIndex++;
+        }
+        else {
+            clearInterval(interval);
+        }
+    }, 3000);
+}
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', initApp);
+}
+else {
+    initApp();
+}
