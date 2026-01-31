2ccfa552a8985f80d6e0eaa9a43d647918bdb2e0
quantumVector <quantum@MacBook-Air-som-tillhor-quantumVector.local>
2026-01-31 20:43:56 +0300

    some update

--- src/lcp.ts
+++ src/lcp.ts
@@ -2,8 +2,8 @@ function initApp(): void {
     const elements = [
         document.getElementById('elem-1'),
         document.getElementById('elem-3'),
-        document.getElementById('elem-4'),
-        document.getElementById('elem-2')
+        document.getElementById('elem-2'),
+        document.getElementById('elem-4')
     ];
 
     let currentIndex = 0;
